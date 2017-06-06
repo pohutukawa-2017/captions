7443ba6b2bf8d9fbb9c0fd74a28ca1a24d84bb12
EDA <eda@Mac-Mini-4.local>
2017-06-07 11:45:52 +1200

    Initial setup for ImageThumbnail

---
+++ client/components/ImageThumbnail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageThumbnail = (props) => {
+  return (
+    <div className='image-thumbnail-container'>
+     <a href='#'><img src={this.props.Imagepath} /></a>
+    </div>
+  )
+}
+
+export default ImageThumbnail
