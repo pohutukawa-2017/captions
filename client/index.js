d003f4f7e1f5fefd7bf6b0692f939a576d3eae39
EDA <eda@mac-mini-5.local>
2017-06-07 10:53:09 +1200

    initial setup

--- client/index.js
+++ client/index.js
@@ -4,19 +4,20 @@ import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
-import reducers from './reducers'
+// import reducers from './reducers'
 import App from './components/App'
 
-let store = createStore(reducers, compose(
-  applyMiddleware(thunkMiddleware),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-))
+// let store = createStore(reducers, compose(
+//   applyMiddleware(thunkMiddleware),
+//   window.devToolsExtension ? window.devToolsExtension() : f => f
+// ))
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <Provider store={store}>
+    // <Provider store={store}>
       <App />
-    </Provider>,
+    /*</Provider>*/
+    ,
     document.getElementById('app')
   )
 })
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var path = require('path')
 var express = require('express')
 
-var redditRoutes = require('./reddit')
+
 var server = express()
 
 server.use(express.static(path.join(__dirname, '../public')))
