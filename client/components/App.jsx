29948bb238b6dfb23fee1bb0ff8ba1392a545889
keewhy <samhoulahan625@gmail.com>
2017-06-08 11:39:29 +1200

    fixed review issues

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ImageCaption from './ImageCaption'
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import ImageContainer from './ImageContainer'
 
 const App = () => (
--- client/components/CaptionListItem.jsx
+++ client/components/CaptionListItem.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 function CaptionListItem (props) {
-  console.log(props)
   return (
     <Link to={`/images/${props.image_id}/${props.id}`}><p>{props.caption}</p></Link>
   )
