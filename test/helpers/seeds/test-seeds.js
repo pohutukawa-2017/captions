8555b4a307fee995bf83449120e8e4fa14c874c4
Don Smith <don@antiantonym.com>
2017-06-07 15:43:55 +1200

    remove user table

--- test/helpers/seeds/test-seeds.js
+++ test/helpers/seeds/test-seeds.js
@@ -1,12 +1,10 @@
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users').del()
+  return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert([
-        {id: 1, username: 'Luke', password: 'luke', profile_pic: ''},
-        {id: 2, username: 'Rory', password: 'rory', profile_pic: ''},
-        {id: 3, username: 'Don', password: 'don', profile_pic: ''}
+      return knex('table_name').insert([
+        {id: 1, column_1_name: 'value', column_2_name: 'value'}
       ])
     })
 }
