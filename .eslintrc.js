f34f855e0cc2f980cadc96dd303d552f84aa0452
Tony Luisi <t.luisi@gmail.com>
2017-06-08 10:11:01 +1200

    added eslinter config to turn off prop types rules

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": [
+    "standard",
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "plugins": [
+    "standard",
+    "promise",
+    "react"
+  ],
+  "rules": {
+    "react/prop-types": "off",
+  }
+};
