d382b95078bb2e910a1a51b2fbafb9977ecc8726
Tony Luisi <t.luisi@gmail.com>
2017-06-08 15:57:24 +1200

    made linter file comply with its own damn linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  "extends": [
-    "standard",
-    "plugin:react/recommended"
+  'extends': [
+    'standard',
+    'plugin:react/recommended'
   ],
-  "plugins": [
-    "standard",
-    "promise",
-    "react"
+  'plugins': [
+    'standard',
+    'promise',
+    'react'
   ],
-  "rules": {
-    "react/prop-types": "off",
-    "object-curly-spacing": ["error", "never"],
-    "arrow-parens": ["error", "always"]
+  'rules': {
+    'react/prop-types': 'off',
+    'object-curly-spacing': ['error', 'never'],
+    'arrow-parens': ['error', 'always']
   }
-};
+}
