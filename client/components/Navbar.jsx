fe574ed084d4ac4d02c51a4f3c2f111282f665d7
Rob <superserva@gmail.com>
2017-06-09 14:32:47 +1200

    Styled Navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,16 +3,27 @@ import {connect} from 'react-redux'
 
 const Navbar = (props) => {
   return (
-    <div className='navbar-container'>
-      <div className='site-name'> <a href='/'><h1>Captions</h1></a></div>
-      <div className='site-menu'>
-        <ul className='menu'>
-          {!props.auth.isAuthenticated && (<li><a href='/register'>Register</a></li>)}
-          {!props.auth.isAuthenticated && (<li><a href='/login'>Sign in</a></li>)}
-          {props.auth.isAuthenticated && (<li>Hi {props.auth.user.username}!</li>)}
-        </ul>
+    <nav className='navbar navbar-default'>
+      <div className='container'>
+        <div className='navbar-header'>
+          <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>
+            <span className='sr-only'>Toggle navigation</span>
+            <span className='icon-bar'></span>
+            <span className='icon-bar'></span>
+            <span className='icon-bar'></span>
+          </button>
+        <a href='/'><img src='/images/captions-logo.jpg' /></a>
+        </div>
+        <div id='navbar' className='navbar-collapse collapse'>
+
+          <ul className='nav navbar-nav navbar-right'>
+            <li><a href='/register'>Register</a></li>
+            <li><a href='/login'>Sign in</a></li>
+            {props.auth.isAuthenticated && (<li>Hi {props.auth.user.username}!</li>)}
+          </ul>
+        </div>
       </div>
-    </div>
+    </nav>
   )
 }
 
